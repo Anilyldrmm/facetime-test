eb33031bd84974622d8d922da9c4b7063bbf2e9e
Anıl Yıldırım <56635776+Anilyldrmm@users.noreply.github.com>
2025-09-07 16:53:42 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  socket.on('offer', (data) => {
+    socket.broadcast.emit('offer', data);
+  });
+
+  socket.on('answer', (data) => {
+    socket.broadcast.emit('answer', data);
+  });
+
+  socket.on('ice-candidate', (data) => {
+    socket.broadcast.emit('ice-candidate', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
